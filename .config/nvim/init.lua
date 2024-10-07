98a1d01d41b28a0df56fe1c69712c427ba70784b
isaac-westaway <i.westaway3119@gmail.com>
2024-10-07 21:45:50 +1000

    fix typo in gitignore

---
+++ .config/nvim/init.lua
@@ -0,0 +1,88 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  	if vim.v.shell_error ~= 0 then
+    		vim.api.nvim_echo({
+      			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      			{ out, "WarningMsg" },
+      			{ "\nPress any key to exit..." },
+    			}, true, {})
+    		vim.fn.getchar()
+    		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- This does not work very well, will fix later
+-- require 'lspconfig'.zls.setup{}
+
+require("lazy").setup({
+  	spec = {
+		{
+			"nvim-tree/nvim-tree.lua",
+			version = "*",
+			lazy = false,
+			dependencies = {
+    				"nvim-tree/nvim-web-devicons",
+  			},
+  			config = function()
+    				require("nvim-tree").setup {}
+  			end,
+		},
+		{
+  			'rmagatti/auto-session',
+  			lazy = false,
+  			
+  			opts = {
+    				suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    				-- log_level = 'debug',
+				enabled = true,
+  			}
+		},
+		{
+
+  			'nvimdev/dashboard-nvim',
+  			event = 'VimEnter',
+  			config = function()
+    				require('dashboard').setup {}
+  			end,
+  			dependencies = { {'nvim-tree/nvim-web-devicons'}}
+		},
+		{
+    			'nvim-telescope/telescope.nvim', 
+			tag = '0.1.8',
+      			dependencies = { 'nvim-lua/plenary.nvim' }
+    		},
+		{
+    			"nvim-treesitter/nvim-treesitter",
+    			build = ":TSUpdate",
+    			config = function ()
+      				local configs = require("nvim-treesitter.configs")	
+      				configs.setup({
+          				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          				sync_install = false,
+          				highlight = { enable = true },
+          				indent = { enable = true },
+        			})
+    			end
+		 }
+  	},
+  	install = { colorscheme = { "habamax" } },
+	checker = { enabled = false },
+})
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set( {'n', 'i', 'v'}, '<C-b>', function() require("nvim-tree.api").tree.toggle({
+										path = nil,
+										current_window = false,
+										find_file = false,
+										update_root = false,
+									}) end)
+
+vim.keymap.set( {'n', 'i', 'v'}, '<C-t>', '<esc><cmd>tabnew<cr>', { silent = true })
+vim.keymap.set( {'n', 'v'}, '<C-f>', builtin.live_grep, { desc = 'Telescope find files'} )
