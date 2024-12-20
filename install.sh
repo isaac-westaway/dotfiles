0c7d11a9dc80359672d9d3d990ca600f08c989da
isaac-westaway <i.westaway3119@gmail.com>
2024-12-21 03:46:00 +1000

    change

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mv ~/dotfiles/configs ~/configs &&
+rm -rf dotfiles 
+
+ln -s ~/configs/vim/.vimrc ~/.vimrc &&
+ln -s ~/configs/vim/.vim ~/.vim &&
+ln -s ~/configs/zsh/.zshrc ~/.zshrc &&
+ln -s ~/configs/zsh/zsh ~/zsh
+
+cd ~ &&
+mkdir build_files &&
+cd build_files &&
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
