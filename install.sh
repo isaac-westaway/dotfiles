c8cc6440c8841fb15d683d9a79ed1a52da016d48
isaac-westaway <i.westaway3119@gmail.com>
2024-12-21 03:10:39 +0000

    fixes

--- install.sh
+++ install.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
 
-#test
+# clean up github clonse
+mv ~/dotfiles/configs ~/configs && rm -rf dotfiles 
 
-mv ~/dotfiles/configs ~/configs &&
-rm -rf dotfiles 
 
-ln -s ~/configs/vim/.vimrc ~/.vimrc &&
-ln -s ~/configs/vim/.vim ~/.vim &&
-ln -s ~/configs/zsh/.zshrc ~/.zshrc &&
+echo "updating symlinks"
+
+[ -L ~/.vimrc ] && rm ~/.vimrc
+ln -s ~/configs/vim/.vimrc ~/.vimrc
+
+[ -L ~/.vim ] && rm -rf ~/.vim
+ln -s ~/configs/vim/.vim ~/.vim
+
+[ -L ~/.zshrc ] && rm .zshrc	
+ln -s ~/configs/zsh/.zshrc ~/.zshrc
+
+[ -L ~/zsh ] && rm -f ~/zsh
 ln -s ~/configs/zsh/zsh ~/zsh
 
-cd ~ &&
-mkdir build_files &&
-cd build_files &&
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git &&
+echo "doing build files"
+if [ -d ~/build_files]; then
+	cd ~/build_files
+else
+	mkdir ~/build_files
+fi
+
+if [ ! zsh-autocomplete ]; then
+	git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+else
+	echo "zsh autocomplete exists"
+fi
+		
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
